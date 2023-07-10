0107ca35202603a52e68f319ec7123440cc4519b
srwesteban <sr.w@hotmail.com>
2023-07-10 00:58:30 -0500

    ejercicio1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte numberByte = 1;
+        int numberInt = 2;
+        float numberFloat = 3.0F;
+        long numberLong = 123213;
+        boolean decision = true;
+        String name = "william";
+
+        System.out.println(numberByte);
+        System.out.println(numberInt);
+        System.out.println(numberFloat);
+        System.out.println(numberLong);
+        System.out.println(decision);
+        System.out.println(name);
+
+
+
+    }
+}
